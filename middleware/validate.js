0efb08d3c21b19a530be9e90cfc2c7bfd9fbd48a
Stewart Rowley <78276704+stewartrowley@users.noreply.github.com>
2022-12-05 11:25:50 -0700

    Added route checking in

--- middleware/validate.js
+++ middleware/validate.js
@@ -24,6 +24,30 @@ const saveRosterMember = (req, res, next) => {
   });
 };
 
+const saveTeamStats = (req, res, next) => {
+  const validationRule = {
+    teamName: "required|string",
+    goalsFor: "required|integer",
+    goalsAgainst: "required|integer",
+    wins: "required|integer",
+    losses: "required|integer",
+    overtimeLosses: "required|integer",
+    pointTotal: "required|integer",
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: "Validation failed",
+        data: err,
+      });
+    } else {
+      next();
+    }
+  });
+};
+
 module.exports = {
   saveRosterMember,
+  saveTeamStats
 };
